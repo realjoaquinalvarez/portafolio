c228d82a05325f6d8fd9fe5abba1cf8c3ccf96ce
Miguel <mmirandasanchez16@gmail.com>
2025-02-15 20:26:30 -0300

    add btn and hanlder

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,7 +1,12 @@
 export default function Info() {
+  const funcionRancia = () => {
+    console.log("working from info.tsx");
+  };
   return (
     <div>
       <h1>About Us</h1>
+
+      <button onClick={funcionRancia}></button>
     </div>
   );
 }
