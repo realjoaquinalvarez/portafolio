75124a740699f0750bdee49db4cdadd9010c7904
Miguel <mmirandasanchez16@gmail.com>
2025-02-16 09:28:03 -0300

    Add BlurFade animation to contact paga

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,15 +1,17 @@
-import { Menu } from 'lucide-react';
-import { CATEGORIES } from '@/constants/categories';
-import { useProjectStore } from '@/store/useProjectStore';
-import { Link } from 'react-router-dom';
+import { Menu } from "lucide-react";
+import { CATEGORIES } from "@/constants/categories";
+import { useProjectStore } from "@/store/useProjectStore";
+import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
-  const { isSidebarCollapsed, toggleSidebar, filters, toggleCategory } = useProjectStore();
+  const { isSidebarCollapsed, toggleSidebar, filters, toggleCategory } =
+    useProjectStore();
 
   return (
     <div
-      className={`fixed top-0 left-0 h-screen transition-all duration-300 ${isSidebarCollapsed ? 'w-16' : 'w-16 md:w-64'
-        }`}
+      className={`fixed top-0 left-0 h-screen transition-all duration-300 ${
+        isSidebarCollapsed ? "w-16" : "w-16 md:w-64"
+      }`}
     >
       <div className="relative flex h-full flex-col border-r border-gray-100 bg-white/50 backdrop-blur-sm">
         <button
@@ -18,26 +20,31 @@ export const Sidebar = () => {
         >
           <Menu className="h-4 w-4 text-gray-600" />
         </button>
-
         <div className="p-4">
-          <h2 className={`text-sm font-medium text-gray-900 ${isSidebarCollapsed ? 'hidden' : 'hidden md:block'}`}>
+          <h2
+            className={`text-sm font-medium text-gray-900 ${
+              isSidebarCollapsed ? "hidden" : "hidden md:block"
+            }`}
+          >
             Categorías
           </h2>
         </div>
-
         <nav className="flex-1 space-y-1 p-1 flex flex-col justify-between">
           <div>
             {CATEGORIES.map((category) => (
               <button
                 key={category.id}
                 onClick={() => toggleCategory(category.id)}
-                className={`w-full flex justify-start items-center space-x-3 px-3 py-2 rounded-md transition-colors ${filters.selectedCategory === category.id
-                  ? 'bg-gray-50 text-gray-900'
-                  : 'text-gray-600 hover:bg-gray-50/50'
-                  }`}
+                className={`w-full flex justify-start items-center space-x-3 px-3 py-2 rounded-md transition-colors ${
+                  filters.selectedCategory === category.id
+                    ? "bg-gray-50 text-gray-900"
+                    : "text-gray-600 hover:bg-gray-50/50"
+                }`}
               >
                 <span className="text-xl">{category.icon}</span>
-                {!isSidebarCollapsed && <span className='hidden md:block'>{category.name}</span>}
+                {!isSidebarCollapsed && (
+                  <span className="hidden md:block">{category.name}</span>
+                )}
               </button>
             ))}
           </div>
@@ -46,8 +53,16 @@ export const Sidebar = () => {
           <Link to="/" className="mt-auto">
             <div className="flex items-center m-[-8px] py-2 rounded-md transition-colors hover:bg-gray-50/50">
               <img
-                className={`h-7 w-7 mb-4 ${isSidebarCollapsed ? 'bg-gray-900 border-gray-00 rounded-lg w-[2.8rem] h-[2.8rem] pr-[0.65rem] p-[0.50rem] md:ml-[0.8rem] mx-auto' : 'mx-auto md:mx-0 md:ml-6' }`}
-                src={isSidebarCollapsed ? '/icons/white-cropped.svg' : '/icons/back-cropped.svg'}
+                className={`h-7 w-7 mb-4 ${
+                  isSidebarCollapsed
+                    ? "bg-gray-900 border-gray-00 rounded-lg w-[2.8rem] h-[2.8rem] pr-[0.65rem] p-[0.50rem] md:ml-[0.8rem] mx-auto"
+                    : "mx-auto md:mx-0 md:ml-6"
+                }`}
+                src={
+                  isSidebarCollapsed
+                    ? "/icons/white-cropped.svg"
+                    : "/icons/back-cropped.svg"
+                }
                 alt="volver"
               />
               {!isSidebarCollapsed && (
@@ -57,11 +72,7 @@ export const Sidebar = () => {
               )}
             </div>
           </Link>
-
         </nav>
-
-
-
       </div>
     </div>
   );
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,6 +11,7 @@ import {
 import { ClipLoader } from "react-spinners";
 import { useNavigate } from "react-router-dom";
 import useIsMobile from "@/hooks/useIsMobile";
+import { BlurFade } from "@/components/magicui/blur-fade";
 
 // Function to detect if the device is iOS
 function isIOS() {
@@ -72,138 +73,146 @@ export default function Contact() {
   }, []);
 
   return (
-    <div className="h-screen w-screen container flex">
-      <div className="max-w-2xl mx-auto my-auto p-8 bg-white rounded-lg shadow-none sm:shadow-lg">
-        <div className="mb-6 flex items-center">
-          <button
-            onClick={() => navigate("/")}
-            className="text-black hover:bg-gray-100 p-2 rounded-full transition duration-300"
-          >
-            <FaArrowLeft />
-          </button>
-          <h2 className="text-3xl font-bold ml-4 text-black">Contact Me</h2>
-        </div>
-
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700 mb-1"
+    <BlurFade delay={0.2}>
+      <div className="h-screen w-screen container flex">
+        <div className="max-w-2xl mx-auto my-auto p-8 bg-white rounded-lg shadow-none sm:shadow-lg">
+          <div className="mb-6 flex items-center">
+            <button
+              onClick={() => navigate("/")}
+              className="text-black hover:bg-gray-100 p-2 rounded-full transition duration-300"
             >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black"
-              required
-            />
+              <FaArrowLeft />
+            </button>
+            <BlurFade delay={0.3}>
+              <h2 className="text-3xl font-bold ml-4 text-black">Contact Me</h2>
+            </BlurFade>
           </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="message"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              rows={4}
-              className="w-full px-3 py-2 border border-gray-300 resize-none rounded-md focus:outline-none focus:ring-1 focus:ring-black"
-              required
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800 transition duration-300 flex items-center justify-center"
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <ClipLoader color="#ffffff" size={20} />
-            ) : (
-              "Send Message"
-            )}
-          </button>
-        </form>
-
-        <div className="flex flex-wrap justify-center items-center gap-6 mt-8">
-          <a
-            href="https://www.linkedin.com/in/realjoaquinalvarez/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-black hover:text-gray-600 transition duration-300"
-          >
-            <FaLinkedin className="text-2xl" />
-          </a>
-          <a
-            href="https://github.com/realjoaquinalvarez"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-black hover:text-gray-600 transition duration-300"
-          >
-            <FaGithub className="text-2xl" />
-          </a>
-          <a
-            href="https://wa.link/v55aj6"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-black hover:text-gray-600 transition duration-300"
-          >
-            <FaWhatsapp className="text-2xl" />
-          </a>
-          {/* Email buttons */}
-          <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2">
-            <div className="relative">
+          <BlurFade delay={0.4}>
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  rows={4}
+                  className="w-full px-3 py-2 border border-gray-300 resize-none rounded-md focus:outline-none focus:ring-1 focus:ring-black"
+                  required
+                ></textarea>
+              </div>
               <button
-                onClick={copyEmail}
-                className="flex items-center space-x-2 bg-gray-100 hover:bg-gray-200 text-black py-1 px-3 rounded-full transition duration-300"
+                type="submit"
+                className="w-full bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800 transition duration-300 flex items-center justify-center"
+                disabled={isLoading}
               >
-                <FaEnvelope className="text-lg" />
-                <span className="text-sm">clcflecha@gmail.com</span>
-                <FaCopy className="text-sm" />
+                {isLoading ? (
+                  <ClipLoader color="#ffffff" size={20} />
+                ) : (
+                  "Send Message"
+                )}
               </button>
-              {copied && (
-                <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-black text-white text-sm px-2 py-1 rounded">
-                  Copied!
+            </form>
+          </BlurFade>
+
+          {/* Footer Side */}
+          <BlurFade delay={0.5}>
+            <div className="flex flex-wrap justify-center items-center gap-6 mt-8">
+              <a
+                href="https://www.linkedin.com/in/realjoaquinalvarez/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-gray-600 transition duration-300"
+              >
+                <FaLinkedin className="text-2xl" />
+              </a>
+              <a
+                href="https://github.com/realjoaquinalvarez"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-gray-600 transition duration-300"
+              >
+                <FaGithub className="text-2xl" />
+              </a>
+              <a
+                href="https://wa.link/v55aj6"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-gray-600 transition duration-300"
+              >
+                <FaWhatsapp className="text-2xl" />
+              </a>
+              {/* Email buttons */}
+              <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2">
+                <div className="relative">
+                  <button
+                    onClick={copyEmail}
+                    className="flex items-center space-x-2 bg-gray-100 hover:bg-gray-200 text-black py-1 px-3 rounded-full transition duration-300"
+                  >
+                    <FaEnvelope className="text-lg" />
+                    <span className="text-sm">clcflecha@gmail.com</span>
+                    <FaCopy className="text-sm" />
+                  </button>
+                  {copied && (
+                    <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-black text-white text-sm px-2 py-1 rounded">
+                      Copied!
+                    </div>
+                  )}
                 </div>
-              )}
+                <a
+                  // For mobile users, open the Gmail app directly; for PC, open the web version
+                  href={getGmailLink(isMobile)}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center space-x-2 bg-gray-100 hover:bg-gray-200 text-black py-1 px-3 rounded-full transition duration-300"
+                >
+                  <span className="text-sm">Go to Gmail</span>
+                  <FaExternalLinkAlt className="text-sm" />
+                </a>
+              </div>
             </div>
-            <a
-              // For mobile users, open the Gmail app directly; for PC, open the web version
-              href={getGmailLink(isMobile)}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center space-x-2 bg-gray-100 hover:bg-gray-200 text-black py-1 px-3 rounded-full transition duration-300"
-            >
-              <span className="text-sm">Go to Gmail</span>
-              <FaExternalLinkAlt className="text-sm" />
-            </a>
-          </div>
+          </BlurFade>
         </div>
       </div>
-    </div>
+    </BlurFade>
   );
 }
