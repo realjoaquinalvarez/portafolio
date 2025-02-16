272f514028397e94e8e268db91306a86531f3a19
Miguel <mmirandasanchez16@gmail.com>
2025-02-15 22:55:36 -0300

    resize-none added

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -132,7 +132,7 @@ export default function Contact() {
               value={formData.message}
               onChange={handleChange}
               rows={4}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black"
+              className="w-full px-3 py-2 border border-gray-300 resize-none rounded-md focus:outline-none focus:ring-1 focus:ring-black"
               required
             ></textarea>
           </div>
